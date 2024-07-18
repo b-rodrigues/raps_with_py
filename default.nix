7fbb91d6e331b4f5bde1007dbfa1cfeacdcea994
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-07-18 15:03:21 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   inherit (pkgs.rPackages) quarto ggplot2 knitr rmarkdown withr;
  };
  pypkgs = builtins.attrValues {
-   inherit (pkgs.python312Packages) plotnine beautifoulsoup4 polars;
+   inherit (pkgs.python312Packages) plotnine beautifulsoup4 polars;
  };
  tex = (pkgs.texlive.combine {
    inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
