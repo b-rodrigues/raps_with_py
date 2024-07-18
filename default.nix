1e709a3950d409ce12bb931e2fa3b70b3b014660
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-07-18 14:52:01 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
 let
  pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/d16116afd7ab80a3eb3312b3bc96fa7bba383638.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) quarto;
-};
-  tex = (pkgs.texlive.combine {
-  inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
-});
+  inherit (pkgs.rPackages) quarto ggplot2 knitr rmarkdown withr;
+ };
+ pypkgs = builtins.attrValues {
+   inherit (pkgs.python312Packages) plotnine beautifoulsoup4 polars;
+ };
+ tex = (pkgs.texlive.combine {
+   inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
+  });
  system_packages = builtins.attrValues {
-  inherit (pkgs) R glibcLocalesUtf8 quarto;
+  inherit (pkgs) R glibcLocalesUtf8 quarto python312;
 };
   in
   pkgs.mkShell {
