f4fdf93db126a3b55a0c863ea36cfcfb9e3cc9b9
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-07-19 10:54:00 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/d16116afd7ab80a3eb3312b3bc96fa7bba383638.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) quarto ggplot2 knitr rmarkdown withr;
+  inherit (pkgs.rPackages) quarto ggplot2 knitr reticulate rmarkdown withr;
  };
  pypkgs = builtins.attrValues {
    inherit (pkgs.python312Packages) plotnine beautifulsoup4 polars;
